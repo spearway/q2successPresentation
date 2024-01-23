31f6bd62a423b033c3b27c02e69a53601a3ba5c2
Pierre Frisch <pierre.frisch@spearway.com>
2024-01-23 14:22:37 +0100

    Added the xcode scheme

--- Package.swift
+++ Package.swift
@@ -18,17 +18,19 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
+        // .package(path: "q2successStudent"),
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:spearway/q2succesStudent.git", from: "1.0.0"),
+        .package(url: "git@github.com:spearway/q2successStudent.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "q2successPresentation",
-            dependencies: []),
+            dependencies: ["q2successStudent"],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "q2successPresentationTests",
-            dependencies: ["q2successPresentation"]),
+            dependencies: ["q2successPresentation", "q2successStudent"]),
     ]
 )
